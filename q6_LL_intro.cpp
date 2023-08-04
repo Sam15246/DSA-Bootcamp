2eedc92565650e11dad78f229fa416908b43acd8
Syed Ali Mujtaba <100562944+Sam15246@users.noreply.github.com>
2023-08-05 01:27:01 +0530

    Update q6_LL_intro.cpp

--- q6_LL_intro.cpp
+++ q6_LL_intro.cpp
@@ -28,5 +28,34 @@ int main()
         temp = temp->next;
     }
 
+    
+    // Dynamic declaration
+    node* n1 = new node{'H', nullptr};
+    node* n2 = new node{'E', nullptr};
+    node* n3 = new node{'L', nullptr};
+    node* n4 = new node{'L', nullptr};
+    node* n5 = new node{'O', nullptr};
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+
+    temp* current = n1;
+    while (temp != nullptr)
+    {
+        cout << temp->x;
+        temp = temp->next;
+    }
+    cout << endl;
+
+    
+    delete n1;
+    delete n2;
+    delete n3;
+    delete n4;
+    delete n5;
+    delete temp;
+
     return 0;
 }
