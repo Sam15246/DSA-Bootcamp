813d7ba6cac33da866623cd29f95e1698df9569c
Syed Ali Mujtaba <100562944+Sam15246@users.noreply.github.com>
2023-08-05 08:26:59 +0530

    Update q6_LL_intro.cpp

--- q6_LL_intro.cpp
+++ q6_LL_intro.cpp
@@ -9,7 +9,7 @@ struct node
 
 int main()
 {
-
+    // Static declaration
     node n1 = {'H', nullptr};
     node n2 = {'E', nullptr};
     node n3 = {'L', nullptr};
@@ -21,41 +21,42 @@ int main()
     n3.next = &n4;
     n4.next = &n5;
 
-    node *temp = &n1;
-    while (temp != nullptr)
+    node *temp1 = &n1;
+    while (temp1 != nullptr)
     {
-        cout << temp->x;
-        temp = temp->next;
+        cout << temp1->x;
+        temp1 = temp1->next;
     }
 
-    
-    // Dynamic declaration
-    node* n1 = new node{'H', nullptr};
-    node* n2 = new node{'E', nullptr};
-    node* n3 = new node{'L', nullptr};
-    node* n4 = new node{'L', nullptr};
-    node* n5 = new node{'O', nullptr};
-
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = n5;
-
-    temp* current = n1;
-    while (temp != nullptr)
-    {
-        cout << temp->x;
-        temp = temp->next;
-    }
     cout << endl;
 
-    
-    delete n1;
-    delete n2;
-    delete n3;
-    delete n4;
-    delete n5;
-    delete temp;
+    // Dynamic declaration
+    node *dn1 = new node{'H', nullptr};
+    node *dn2 = new node{'E', nullptr};
+    node *dn3 = new node{'L', nullptr};
+    node *dn4 = new node{'L', nullptr};
+    node *dn5 = new node{'O', nullptr};
+
+    dn1->next = dn2;
+    dn2->next = dn3;
+    dn3->next = dn4;
+    dn4->next = dn5;
+
+    node *temp2 = dn1;
+    while (temp2 != nullptr)
+    {
+        cout << temp2->x;
+        temp2 = temp2->next;
+    }
+
+    cout << endl;
+
+    // Delete only the dynamically allocated nodes
+    delete dn1;
+    delete dn2;
+    delete dn3;
+    delete dn4;
+    delete dn5;
 
     return 0;
 }
