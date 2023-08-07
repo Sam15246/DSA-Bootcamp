d352c7f020675a2f1a18144c9fb12ef6b7eb6038
Syed Ali Mujtaba <100562944+Sam15246@users.noreply.github.com>
2023-08-07 20:02:12 +0530

    Update q7_LL_manipulation.cpp

--- q7_LL_manipulation.cpp
+++ q7_LL_manipulation.cpp
@@ -174,14 +174,71 @@ void display(ListNode *head) // Display using List Traversal
     }
 }
 
+int readValue(ListNode *head, int size, int index)
+{
+    if (index < 0 || index >= size)
+    {
+        // Invalid Index
+        return -1;
+    }
+
+    ListNode *temp = head;
+    while (index > 0)
+    {
+        temp = temp->next;
+        index--;
+    }
+
+    return temp->data;
+}
+
+int writeValue(ListNode *head, int size, int index, int value)
+{
+    if (index < 0 || index >= size)
+    {
+        // Invalid Index
+        return -1;
+    }
+
+    ListNode *temp = head;
+    while (index > 0)
+    {
+        temp = temp->next;
+        index--;
+    }
+
+    temp->data = value;
+    return temp->data;
+}
+
+int searchValue(ListNode *head, int value)
+{
+    int index = 0;
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == value)
+        {
+            return index;
+        }
+        temp = temp->next;
+        index++;
+    }
+    return -1;
+}
+
 int main()
 {
     int choice, index, value;
-    ListNode *head = NULL;
+    ListNode *head
+
+        = NULL;
     while (1)
     {
         cout << "\n\n=============MENU=============\n";
-        cout << "\nInsert at:\n\t01.beginning\n\t02.end\n\t03.given index\nDelete at:\n\t04.beginning\n\t05.end\n\t06.given index\n0.Exit\n\n";
+        cout << "\nInsert at:\n\t01.beginning\n\t02.end\n\t03.given index\nDelete at:\n\t04.beginning\n\t05.end\n\t06.given index\n";
+        cout << "\nOther Operations:\n\t07.Read value at index\n\t08.write to given index\n\t09.search value\n";
+        cout << "0.Exit\n\n";
         cin >> choice;
         cout << "\n";
         switch (choice)
@@ -190,11 +247,11 @@ int main()
         {
             cout << "Enter data value: ";
             cin >> value;
-            index = insAtBeg(head, value);
+            int index = insAtBeg(head, value);
             if (index < 0)
-                cout << "-- Node can't be inserted at the specified index"<< endl;
+                cout << "-- Node can't be inserted at index " << index << endl;
             else
-                cout << "++ Node is inserted at the specified index"<< endl;
+                cout << "++ Node is inserted at index " << index << endl;
             display(head);
             break;
         }
@@ -202,11 +259,11 @@ int main()
         {
             cout << "Enter data value: ";
             cin >> value;
-            index = insAtEnd(head, value);
+            int index = insAtEnd(head, value);
             if (index < 0)
-                cout << "-- Node can't be inserted at the specified index"<< endl;
+                cout << "-- Node can't be inserted at index " << index << endl;
             else
-                cout << "++ Node is inserted at the specified index"<< endl;
+                cout << "++ Node is inserted at index " << index << endl;
             display(head);
             break;
         }
@@ -216,31 +273,31 @@ int main()
             cin >> value;
             cout << "Enter index: ";
             cin >> index;
-            index = insAtIndex(head, value, index);
-            if (index < 0)
-                cout << "-- Node can't be inserted at the specified index"<< endl;
+            int newIndex = insAtIndex(head, value, index);
+            if (newIndex < 0)
+                cout << "-- Node can't be inserted at index " << index << endl;
             else
-                cout << "++ Node is inserted at the specified index"<< endl;
+                cout << "++ Node is inserted at index " << newIndex << endl;
             display(head);
             break;
         }
         case 4: // delete at beg
         {
-            index = delAtBeg(head);
-            if (index < 0)
-                cout << "-- Node can't be deleted at the specified index"<< endl;
+            int data = delAtBeg(head);
+            if (data < 0)
+                cout << "-- Node can't be deleted at index " << index << endl;
             else
-                cout << "++ Node is deleted at the specified index"<< endl;
+                cout << "++ Node with data " << data << " is deleted from the beginning" << endl;
             display(head);
             break;
         }
         case 5: // delete at end
         {
-            index = delAtEnd(head);
-            if (index < 0)
-                cout << "-- Node can't be deleted at the specified index"<< endl;
+            int data = delAtEnd(head);
+            if (data < 0)
+                cout << "-- Node can't be deleted at index " << index << endl;
             else
-                cout << "++ Node is deleted at the specified index"<< endl;
+                cout << "++ Node with data " << data << " is deleted from the end" << endl;
             display(head);
             break;
         }
@@ -250,10 +307,46 @@ int main()
             cin >> index;
             int data = delAtIndex(head, index);
             if (data < 0)
-                cout << "-- Node can't be deleted at the specified index"<< endl;
+                cout << "-- Node can't be deleted at index " << index << endl;
             else
-                cout << "++ Node with data " << data << " is deleted at the specified index"<< endl;
+                cout << "++ Node with data " << data << " is deleted from index " << index << endl;
             display(head);
+            break;
+        }
+        case 7: // read value at index
+        {
+            cout << "Enter index: ";
+            cin >> index;
+            int value = readValue(head, sizeOf(head), index);
+            if (value < 0)
+                cout << "-- Invalid index " << index << endl;
+            else
+                cout << "Value at index " << index << " is " << value << endl;
+            break;
+        }
+        case 8: // write value at index
+        {
+            cout << "Enter index: ";
+            cin >> index;
+            cout << "Enter value: ";
+            cin >> value;
+            int newValue = writeValue(head, sizeOf(head), index, value);
+            if (newValue < 0)
+                cout << "-- Invalid index " << index << endl;
+            else
+                cout << "Value at index " << index << " is updated to " << newValue << endl;
+            display(head);
+            break;
+        }
+        case 9: // search value
+        {
+            cout << "Enter value to search: ";
+            cin >> value;
+            int index = searchValue(head, value);
+            if (index < 0)
+                cout << "-- Value " << value << " not found in the linked list" << endl;
+            else
+                cout << "Value " << value << " found at index " << index << endl;
             break;
         }
         case 0:
@@ -263,7 +356,7 @@ int main()
         }
         default:
         {
-            cout << "Invalid choice (choose 0-6)";
+            cout << "Invalid choice (choose 0-9)";
         }
         }
     }
